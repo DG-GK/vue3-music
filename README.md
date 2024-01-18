3566b50738de35a73681eadc3ba86f985c986eae
橘子Gz <2622246245@qq.com>
2024-01-18 16:33:54 +0800

    项目筹备启动

---
+++ README.md
@@ -0,0 +1,20 @@
+# 网易云音乐
+Vue3+ElementPlus仿网易云音乐实战项目
+
+## 项目运行命令
+
+```
+cd vue3-music
+npm install
+npm run dev
+```
+
+## 技术栈
+* Vue3.x
+* Element-plus
+* pinia 2.x
+* TypeScript
+* sass
+* Vue Router 4.x
+* axios
+* ES6
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+body,
+html,
+#app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  * {
+    outline: none;
+    box-sizing: border-box;
+  }
+}
+</style>
